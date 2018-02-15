ba16d279353df2e9478ebaa78a17b53a0cbd80c5
Daniele Mantovani <daniele.mantovani@uniupo.it>
2018-02-15 14:39:42 +0100

    First commit

---
+++ Linq2DbSample/Linq2DbSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linq2DbSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
